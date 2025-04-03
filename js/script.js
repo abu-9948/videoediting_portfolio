73fd4bce8636b425cf0a50df0e43d8ec341ee4ff
abu-9948“ <abubakarsiddiqueshaik@ABUs-MacBook-Air.local>
2025-04-03 18:15:35 +0530

    some stylings

--- js/script.js
+++ js/script.js
@@ -1,35 +1,32 @@
-// toggle icon navbar
+// Toggle icon navbar
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active'); 
+    menuIcon.classList.toggle('active'); // Optional, if you want to animate the menu icon
+});
 
-// scroll sections
+// Scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
- 
-    
-
-    // sticky header
+    // Sticky header
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop -100;
+        let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(Links => {
-                Links.classList.remove('active');
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
+            });
         }
-    })
+    });
 
     let header = document.querySelector('header');
-
     header.classList.toggle('sticky', window.scrollY > 100);
-
-
-
-}
+};
